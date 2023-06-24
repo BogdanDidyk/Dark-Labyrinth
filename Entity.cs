04a34532b53b45acb808e5e86f4ffb575147b6e7
Bogdan Didyk <bogdandidyk26@gmail.com>
2023-06-24 23:59:38 +0300

    Added Entity.cs file which contains Entity class

---
+++ Entity.cs
@@ -0,0 +1,16 @@
+namespace Prize_Collector_Console_Game
+{
+    class Entity : Position
+    {
+        public char Symbol { get; set; }
+        public ConsoleColor Color { get; set; }
+
+        public Entity(uint left = 0, uint top = 0, char symbol = '*', ConsoleColor color = ConsoleColor.Gray) : base(left, top)
+        {
+            Symbol = symbol;
+            Color = color;
+        }
+
+        public Entity(Position pos, char symbol = '*', ConsoleColor color = ConsoleColor.Gray) : this(pos.Left, pos.Top, symbol, color) { }
+    }
+}
