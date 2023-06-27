6f0c01d6dbc22fd3e0549c8c9a105b95cbcc5f83
Bogdan Didyk <bogdandidyk26@gmail.com>
2023-06-27 21:48:45 +0300

    Added ChangePositionTo() method to MovableEntity() class

--- MovableEntity.cs
+++ MovableEntity.cs
@@ -4,4 +4,10 @@ namespace Prize_Collector_Console_Game
     {
         public MovableEntity(Position position, char symbol = '*', ConsoleColor color = ConsoleColor.Gray) : base(position, symbol, color) { }
     }
+	
+	public void ChangePositionTo(Position position)
+	{
+		Left = position.Left;
+		Top = position.Top;
+	}
 }
