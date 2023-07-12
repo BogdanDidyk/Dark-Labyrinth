b556ef38fb1ddd05d4067a7340b00f0d2d7c3148
Bogdan Didyk <bogdandidyk26@gmail.com>
2023-07-12 22:01:41 +0300

    Added Hero class into Hero.cs file

---
+++ Hero.cs
@@ -0,0 +1,7 @@
+namespace Prize_Collector_Console_Game
+{
+	class Hero : Entity
+    {
+        public Hero(char symbol = '*', ConsoleColor color = ConsoleColor.Gray) : base(symbol, color) { }
+    }
+}
