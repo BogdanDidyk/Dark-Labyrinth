3b5e644ad40f1594d6b001d86b12ca42b0861788
Bogdan Didyk <bogdandidyk26@gmail.com>
2023-07-12 22:05:41 +0300

    Added PrizeType enum into Prize.cs file

--- Prize.cs
+++ Prize.cs
@@ -1,7 +1,14 @@
 namespace Prize_Collector_Console_Game
 {
+    public enum PrizeType : uint { Silver = 3, Gold = 8, Diamond = 17 }
+
     class Prize : Entity
     {
-        public Prize(char symbol = '*', ConsoleColor color = ConsoleColor.Gray) : base(symbol, color) { }
+        public PrizeType PrizeType { get; set; }
+
+        public Prize(char symbol = '*', ConsoleColor color = ConsoleColor.Gray, PrizeType prizeType = PrizeType.Silver) : base(symbol, color)
+        {
+            PrizeType = prizeType;
+        }
     }
 }
