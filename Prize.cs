f57a3fc280cd8f460b1be92d694b9fc93702b7c1
Bogdan Didyk <bogdandidyk26@gmail.com>
2023-07-12 22:03:56 +0300

    Added Prize class into Prize.cs file

---
+++ Prize.cs
@@ -0,0 +1,7 @@
+namespace Prize_Collector_Console_Game
+{
+    class Prize : Entity
+    {
+        public Prize(char symbol = '*', ConsoleColor color = ConsoleColor.Gray) : base(symbol, color) { }
+    }
+}
