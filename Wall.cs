f233f7f88cbc419c6dbc0817375e21772c8f364e
Bogdan Didyk <bogdandidyk26@gmail.com>
2023-07-12 22:07:11 +0300

    Added Wall class into Wall.cs file

---
+++ Wall.cs
@@ -0,0 +1,7 @@
+namespace Prize_Collector_Console_Game
+{
+    class Wall : Entity
+    {
+        public Wall(char symbol = '*', ConsoleColor color = ConsoleColor.Gray) : base(symbol, color) { }
+    }
+}
