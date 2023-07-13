365aa79ddd8f185dab530c9203b4f4c47acc8afd
Bogdan Didyk <bogdandidyk26@gmail.com>
2023-07-13 23:24:23 +0300

    Added private fields to Map class that store info about walls and prizes

--- Map.cs
+++ Map.cs
@@ -2,6 +2,9 @@ namespace Prize_Collector_Console_Game
 {
     class Map
     {
-
+        private Dictionary<Position, Wall> walls;
+        private Dictionary<Position, Prize> prizes = new Dictionary<Position, Prize>();
+        private readonly Random rnd;
+		
     }
 }
