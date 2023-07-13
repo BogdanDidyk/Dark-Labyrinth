61f30fb8d7d0e4a758511bf39215dcb24b43059a
Bogdan Didyk <bogdandidyk26@gmail.com>
2023-07-13 23:21:58 +0300

    Changed properties to read only in Position class

--- Position.cs
+++ Position.cs
@@ -2,8 +2,8 @@ namespace Prize_Collector_Console_Game
 {
     struct Position
     {
-        public uint Left { get; set; }
-        public uint Top { get; set; }
+        public uint Left { get; private set; }
+        public uint Top { get; private set; }
 
         public Position(uint left = 0, uint top = 0)
         {
