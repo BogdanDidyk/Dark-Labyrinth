11f8bd98a0d8bc8fa58b692ad0258d5cb8c5c7d0
Bogdan Didyk <bogdandidyk26@gmail.com>
2023-07-15 23:42:01 +0300

    Added auxiliary properties to Map class

--- Map.cs
+++ Map.cs
@@ -6,10 +6,21 @@ namespace Prize_Collector_Console_Game
         private Dictionary<Position, Prize> prizes = new Dictionary<Position, Prize>();
         private readonly Random rnd;
 		
+		public uint Width { get; private set; }
+        public uint Height { get; private set; }
+        public uint WallsCount { get => (uint)walls.Count; }
+        public uint PrizesCount { get => (uint)prizes.Count; }
+        public uint HeroesCount { get => 1u; }
+        public uint FreePositionsCount { get => Width * Height - WallsCount - PrizesCount - HeroesCount; }
+		
 		public Map(Dictionary<Position, Wall> wallPositions)
         {
             rnd = new Random();
             walls = wallPositions;
+			
+			var keys = wallPositions.Keys;
+            Width = keys.Max(wall => wall.Left) + 1;
+            Height = keys.Max(wall => wall.Top) + 1;
         }
     }
 }
