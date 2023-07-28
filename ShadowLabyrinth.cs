9008c3f376f9224ee975e3bcf5951f7518946743
Bogdan Didyk <bogdandidyk26@gmail.com>
2023-07-28 23:11:07 +0300

    Added ShadowLabyrinth class with required properties

--- ShadowLabyrinth.cs
+++ ShadowLabyrinth.cs
@@ -1,4 +1,13 @@
 namespace Prize_Collector_Console_Game
 {
+	class ShadowLabyrinth
+    {
+		private Random rnd = new Random();
 
+        public Map Map { get; private set; }
+        public uint VisibilityRadius { get; private set; }
+        public ConsoleColor DimmingColor { get; private set; }
+        public ConsoleColor LightingColor { get; private set; }
+        public uint Scores { get; private set; }
+	}
 }
